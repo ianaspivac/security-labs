585751ae67036b3b228709ca5f414a3f581bd5c9
Iana Spivac <yspivac@gmail.com>
2021-10-10 17:14:05 +0300

    added audits to repo

---
+++ qt.py
@@ -0,0 +1,187 @@
+from PyQt6.QtWidgets import (QMainWindow, QTextEdit,
+                             QFileDialog, QApplication, QLineEdit, QHBoxLayout, QVBoxLayout, QWidget, QPushButton,
+                             QCheckBox, QScrollArea)
+from PyQt6.QtGui import QIcon, QAction
+from pathlib import Path
+
+from parserFile import mainParse, identifyOptionKeywords
+from searchOption import saveOptions,exportOptions
+import sys
+import json
+
+
+
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.layoutV = QVBoxLayout()
+
+        self.searchInput = QLineEdit()
+        self.btnSearch = QPushButton("Search")
+        self.searchInput.setMaxLength(20)
+        self.searchInput.setPlaceholderText("Search keyword...")
+
+        self.layoutV.addWidget(self.searchInput)
+        self.layoutV.addWidget(self.btnSearch)
+
+        self.layoutH = QHBoxLayout()
+        self.layoutV.addLayout(self.layoutH)
+
+        self.textEdit = QTextEdit()
+
+        self.layoutH.addWidget(self.textEdit)
+
+        self.optionsBox = QVBoxLayout()
+        self.widgetOptionsBox= QWidget()
+        self.widgetOptionsBox.setLayout(self.optionsBox)
+        self.scroll = QScrollArea()
+        self.layoutH.addWidget(self.scroll)
+        self.scroll.setWidget(self.widgetOptionsBox)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setFixedHeight(400)
+
+
+
+        self.widget = QWidget()
+        self.widget.setLayout(self.layoutV)
+        self.setCentralWidget(self.widget)
+        self.statusBar()
+
+        self.btnSearch.clicked.connect(self.find_word)
+
+        openFile = QAction(QIcon('open.png'), 'Open', self)
+        openFile.setShortcut('Ctrl+O')
+        openFile.setStatusTip('Open new File')
+        openFile.triggered.connect(self.showDialog)
+
+        saveFile = QAction(QIcon('open.png'), 'Save', self)
+        saveFile.setShortcut('Ctrl+S')
+        saveFile.setStatusTip('Save current policy json')
+        saveFile.triggered.connect(self.saveDialog)
+
+        saveAsFile = QAction(QIcon('open.png'), 'Save As', self)
+        saveAsFile.setStatusTip('Save current selection json')
+        saveAsFile.triggered.connect(self.saveAsDialog)
+
+        exportFile = QAction(QIcon('open.png'), 'Export', self)
+        exportFile.setStatusTip('Export selection as audit')
+        exportFile.triggered.connect(self.exportDialog)
+
+        verifyFile = QAction(QIcon('open.png'), 'Verify audit', self)
+        verifyFile.setStatusTip('Verify audit')
+        verifyFile.triggered.connect(self.verifyAudit)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(openFile)
+        fileMenu.addAction(saveFile)
+        fileMenu.addAction(saveAsFile)
+        fileMenu.addAction(exportFile)
+        fileMenu.addAction(verifyFile)
+
+        self.setGeometry(200, 100, 1000, 500)
+        self.setWindowTitle('File dialog')
+        self.show()
+
+
+    def showDialog(self):
+        home_dir = str(Path.home())
+        fname = QFileDialog.getOpenFileName(self, 'Open file', home_dir)
+
+        if fname[0]:
+            mainParse(fname[0])
+            parsedFile = open(fname[0].split(".audit")[0] + ".json", 'r')
+            parsed = json.loads(parsedFile.read())
+            readParsedFile = json.dumps(parsed, indent=4, sort_keys=True)
+            self.textEdit.setText(readParsedFile)
+            self.addOptionsSelect()
+
+    def saveDialog(self):
+        home_dir = str(Path.home())
+        name = QFileDialog.getSaveFileName(self, 'Save File', home_dir,"*.json")
+        if name[0]:
+            file = open(name[0], 'w')
+            text = self.textEdit.toPlainText()
+            file.write(text)
+            file.close()
+
+    def exportDialog(self):
+        self.saveCheckbox()
+        fileSave = exportOptions(self.listSelectedOptions, self.textEdit.toPlainText())
+        home_dir = str(Path.home())
+        name = QFileDialog.getSaveFileName(self, 'Export File', home_dir, "*.audit")
+        if name[0]:
+            file = open(name[0], 'w')
+            file.write(fileSave)
+            file.close()
+
+    def saveAsDialog(self):
+        self.saveCheckbox()
+        fileSave = saveOptions(self.listSelectedOptions,self.textEdit.toPlainText())
+        home_dir = str(Path.home())
+        name = QFileDialog.getSaveFileName(self, 'Save File', home_dir,"*.json")
+        if name[0]:
+            file = open(name[0], 'w')
+            file.write(fileSave)
+            file.close()
+
+
+    def verifyAudit(self):
+        print("verify")
+
+    def saveCheckbox(self):
+        self.listSelectedOptions = []
+        for i, v in enumerate(self.listCheckBox):
+            if v.isChecked():
+                self.listSelectedOptions.append(v.text())
+
+    def addOptionsSelect(self):
+
+        self.listCheckBox = identifyOptionKeywords(self.textEdit.toPlainText())
+
+        self.selectBtn = QPushButton("Select all")
+        self.selectBtn.clicked.connect(self.selectOptions)
+        self.deselectBtn = QPushButton("Deselect all")
+        self.deselectBtn.clicked.connect(self.deselectOptions)
+        self.optionsBox.addWidget(self.selectBtn)
+        self.optionsBox.addWidget(self.deselectBtn)
+
+        for i, v in enumerate(self.listCheckBox):
+            self.listCheckBox[i] = QCheckBox(v)
+            self.optionsBox.addWidget(self.listCheckBox[i])
+
+        # self.saveOptionsBtn = QPushButton("Save options")
+        # self.saveOptionsBtn.clicked.connect(self.checkboxState)
+
+        # self.optionsBox.addWidget(self.saveOptionsBtn)
+
+    def deselectOptions(self):
+        for i, v in enumerate(self.listCheckBox):
+            self.listCheckBox[i].setChecked(False)
+
+    def selectOptions(self):
+        for i, v in enumerate(self.listCheckBox):
+            self.listCheckBox[i].setChecked(True)
+
+
+    def find_word(self):
+        words = self.searchInput.text()
+        if not self.textEdit.find(words):
+            cursor = self.textEdit.textCursor()
+            cursor.setPosition(0)
+            self.textEdit.setTextCursor(cursor)
+            self.textEdit.find(words)
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
---
+++ searchOption.py
@@ -0,0 +1,47 @@
+import re
+
+
+
+
+def exportOptions(listKeywords, string):
+    arrayString = string.split()
+    options = []
+    openBracketFlag = False
+    for keyword in listKeywords:
+        openBracketFlag = False
+        for word in arrayString:
+            if keyword + '"' in word:
+                options.append("\n<" + re.sub(r'[0-9]','',word[1:-2]) + ">\n")
+                openBracketFlag = True
+            elif openBracketFlag and ("]" == word or "]," == word):
+                options.append("\n</" + re.sub(r'[0-9]','',keyword) + ">")
+                break;
+            elif openBracketFlag:
+                if "," == word or '",' in word:
+                    options.append(word[:-1]+"\n")
+                elif '[' == word or '{' == word or '}' == word:
+                    continue
+                elif '":' in word:
+                    options.append(word[1:-2]+":")
+                else:
+                    options.append(word)
+
+    return(' '.join(options))
+
+
+def saveOptions(listKeywords, string):
+    arrayString = string.split()
+    options = []
+    openBracketFlag = False
+    for keyword in listKeywords:
+        openBracketFlag = False
+        for word in arrayString:
+            if keyword + '"' in word or openBracketFlag:
+                options.append(word)
+                openBracketFlag = True
+            if openBracketFlag and "]" == word:
+                options.append(",")
+                break;
+            elif openBracketFlag and "]," == word:
+                break;
+    return ("{" + ''.join(options) + "}")
